e5622de03fd46022633743eb6cb962a72829585f
Florencia <florenciadaianabeber@gmail.com>
2016-11-23 14:17:27 -0300

    actualizando

--- bkpadmin/admin/templates/index.tpl
+++
@@ -1,9 +0,0 @@
-
-
-  <br>
-
-  <h1>BIENVENIDO SEÑOR ADMINISTRADOR DEL GIMNASIO!!</h1>
-  <h2>Que tenga un hermoso día!</h2>
-  <br>
-  <img src="../images/happy.jpg" width="500px" class="img-circle">
-  <h3>A continuación encontrarás las herramientas para poder hacer su trabajo..</h3>
--- database/db_gym.sql
+++ database/db_gym.sql
@@ -7,8 +7,10 @@ create table actividad(
 
 create table profesor(
 	id int(10) not null AUTO_INCREMENT,
-	nombre varchar(50) not null,
-	apellido varchar(50) not null,
+	email varchar(200) not null,
+	pass varchar(50) not null,
+	is_admin boolean not null default '1',
+	apyno varchar(50) not null,
 	dni int(10) not null,
 	foto varchar(300) not null,
 	horarios varchar(300),
@@ -26,31 +28,72 @@ create table img_actividad(
 	constraint fk_img foreign key (id_act) references actividad(id) on delete cascade on update cascade
 );
 
-insert into actividad (nombre, descripcion) values
-	('Zumba', 'enfocada por una parte a mantener un cuerpo saludable y por otra a desarrollar, fortalecer y dar flexibilidad al cuerpo mediante movimientos de baile combinados con una serie de rutinas aeróbicas'),
-	('Tae Box','El Tae Bo es una rutina de ejercicios asociada al boxeo y al taekwondo. Se combina la música con ejercicios intensivos'),
-	('Pilates', 'Pilates, es un sistema de entrenamiento físico y mental creado a principios del siglo XX por Joseph Hubertus Pilates'),
-	('Yoga', 'La palabra Yoga significa “unión”. Primero unión con uno mismo, en la integración de todas las capas y estratos que componen la estructura personal.'),
-	('Cross fit', ' sistema de acondicionamiento físico basado en ejercicios constantemente variados, con movimientos funcionales');
+create table usuario(
+	id int(10) not null AUTO_INCREMENT,
+	email varchar(200) not null,
+	pass varchar(50) not null,
+	apyno varchar(50) not null,
+	constraint pk_usuario primary key (id)
+);
 
-insert into profesor (nombre, apellido, dni, foto, horarios, descripcion, id_act) values
-	('Pedro', 'Flores', '1236586425','perfil1.jpg', 'de lunes a viernes de 16hs a 19hs', 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.','1'),
-	('Ramona', 'Gia', '4565436564','perfil2.jpg', 'de lunes a viernes de 16hs a 19hs', 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.','2'),
-	('Coca', 'Sanchez', '47545847','perfil3.jpg', 'de lunes a viernes de 16hs a 19hs', 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.','1');
+create table comentario(
+	id int(10) not null AUTO_INCREMENT,
+	comentario varchar (140) not null,
+	puntaje int(1) not null,
+	id_usuario int(10) not null,
+	id_act int(10) not null,
+	constraint pk_comentario primary key (id),
+	constraint fk_comentario foreign key (id_usuario) references usuario (id) on delete cascade on update cascade,
+	constraint fk_comentario1 foreign key (id_act) references actividad (id) on delete cascade on update cascade
+);
 
+create table inscripcion(
+	id int(10) not null AUTO_INCREMENT,
+	id_usuario int(10) not null,
+	id_profe int(10) not null,
+	constraint pk_inscripcion primary key (id),
+	constraint fk_inscripcion foreign key (id_usuario) references usuario (id) on delete cascade on update cascade,
+	constraint fk_inscripcion1 foreign key (id_profe) references profesor (id) on delete cascade on update cascade
+);
+
+
+/*llenado de tablas*/
+insert into actividad (id,nombre, descripcion) values
+	(1,'none','none'),
+	(2,'Tae Box','El Tae Bo es una rutina de ejercicios asociada al boxeo y al taekwondo. Se combina la música con ejercicios intensivos'),
+	(3,'Pilates', 'Pilates, es un sistema de entrenamiento físico y mental creado a principios del siglo XX por Joseph Hubertus Pilates'),
+	(4,'Yoga', 'La palabra Yoga significa “unión”. Primero unión con uno mismo, en la integración de todas las capas y estratos que componen la estructura personal.'),
+	(5,'Cross fit', ' sistema de acondicionamiento físico basado en ejercicios constantemente variados, con movimientos funcionales'),
+	(6,'Zumba', 'enfocada por una parte a mantener un cuerpo saludable y por otra a desarrollar, fortalecer y dar flexibilidad al cuerpo mediante movimientos de baile combinados con una serie de rutinas aeróbicas');
+
+	
 insert into img_actividad(foto,id_act) values
-	('zumba.jpg',1),
-	('zumba2.jpg',1),
-	('zumba3.jpg',1),
-	('taebox.jpg',2),
-	('taebox2.jpg',2),
-	('taebox3.jpg',2),
-	('pilates.jpg',3),
-	('pilates2.jpg',3),
-	('pilates3.jpg',3),
-	('yoga.jpg',4),
-	('yoga2.jpg',4),
-	('yoga3.jpg',4),
- 	('crossfit.jpg',5),
- 	('crossfit3.jpg',5),
- 	('crossfit4.jpg',5)
+	('zumba.jpg',6),('zumba2.jpg',6),('zumba3.jpg',6),
+	('taebox.jpg',2),('taebox2.jpg',2),('taebox3.jpg',2),
+	('pilates.jpg',3),('pilates2.jpg',3),('pilates3.jpg',3),
+	('yoga.jpg',4),('yoga2.jpg',4),('yoga3.jpg',4),
+ 	('crossfit.jpg',5),	('crossfit2.jpg',5),('crossfit4.jpg',5);
+
+insert into profesor (id,email,pass,is_admin,apyno,dni,foto,horarios,descripcion,id_act) values
+	(1,'admin@admin.com','admin','0','Administrador','12345678','default.jpg','none','none',1),
+	(2,'pepe@gmail.com','pepe','1','Pepe Flores','546472466','perfil1.jpg','lunes y martes de 14 a 15','profesor de bla bla bla',2),
+	(3,'ramona@gmail.com', 'ramona','1', 'Ramona Fernandez', '4565436564','perfil2.jpg', 'de lunes a viernes de 16hs a 19hs', 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.',3),
+	(4,'coca@gmail.com', 'coca', '1', 'Coca Sanchez', '47545847','perfil3.jpg', 'de lunes a viernes de 16hs a 19hs', 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.',3);
+
+insert into usuario(email, pass,apyno) values
+	('flor@gmail.com','flor', 'Florencia Beber'),
+	('carlos@yahoo.com.ar','carlos','Carlos Peralta'),
+	('barney@hotmail.com','barney','Barney el Dinosaurio');
+
+insert into inscripcion(id_usuario,id_profe) values
+	(1,2),(2,2),(3,3);
+
+insert into comentario(comentario,puntaje,id_usuario,id_act) values
+	("me encanta esta actividad", 5, 1, 2),
+	("me encanta esta actividad", 5, 2, 2),
+	("me encanta esta actividad", 5, 3, 2),
+	("no me agrada del todo", 1, 1, 3),
+	("no me agrada del todo", 1, 2, 3),
+	("no me agrada del todo", 1, 3, 3);
+
+
